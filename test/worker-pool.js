e35baff23c882b08ab9b87385b43502731f8a240
wilk <wilk3ert@gmail.com>
2018-09-10 20:52:51 +0200

    add worker pool test suite

---
+++ test/worker-pool.js
@@ -0,0 +1,26 @@
+const { assert } = require('chai')
+const { job, stop } = require('../src/job')
+
+describe('Worker Pool Testing', () => {
+  it('should execute at most 4 jobs at a time', async () => {
+    let error
+    let diff
+
+    const task = () => new Promise(resolve => setTimeout(() => resolve(), 10))
+
+    try {
+      const start = Date.now()
+      await Promise.all([job(task), job(task), job(task), job(task), job(task)])
+      diff = Date.now() - start
+    } catch (err) {
+      error = err
+    }
+
+    assert.isUndefined(error)
+    assert.isAbove(diff, 10 * 2)
+  })
+
+  after(() => {
+    stop()
+  })
+})
