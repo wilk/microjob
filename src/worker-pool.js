b6e693bed274ea50e90e265016b52c4ae544dd89
wilk <wilk3ert@gmail.com>
2018-09-10 08:58:11 +0200

    add working worker pool

--- src/worker-pool.js
+++ src/worker-pool.js
@@ -1,8 +1,14 @@
 const { Worker } = require('worker_threads')
 const os = require('os')
+const EventEmitter = require('events')
 
-class WorkerPool {
+const WORKER_STATE_READY = 'ready'
+const WORKER_STATE_BUSY = 'busy'
+
+class WorkerPool extends EventEmitter {
   constructor(maxWorkers) {
+    super()
+
     this.taskQueue = []
     this.workers = []
 
@@ -10,19 +16,57 @@ class WorkerPool {
       const worker = new Worker('./src/worker.js')
 
       worker.once('online', () => {
-        
-      })
-      worker.once('error', () => {})
+        this.workers.push({
+          status: WORKER_STATE_READY,
+          worker
+        })
 
-      this.workers.push({
-        status: 'idle',
-        worker
+        this.tick()
+      })
+      worker.once('error', () => {
+        // todo: what to do if a thread doesn't come up? retry? raise an exception?
       })
     }
   }
 
-  next() {
+  tick() {
+    if (this.taskQueue.length === 0) return
 
+    let availableWorker = null
+    for (let i = 0; i < this.workers.length; i++) {
+      if (this.workers[i].status === WORKER_STATE_READY) {
+        availableWorker = this.workers[i]
+        break
+      }
+    }
+
+    if (availableWorker === null) return
+
+    const work = this.taskQueue.shift()
+
+    availableWorker.status = WORKER_STATE_BUSY
+    const { worker } = availableWorker
+    this.emit('tick', { work, worker })
+  }
+
+  enqueue({ handler, config, resolve, reject }) {
+    this.taskQueue.push({ handler, config, resolve, reject })
+    this.tick()
+  }
+
+  free(worker) {
+    for (let i = 0; i < this.workers.length; i++) {
+      if (worker.threadId === this.workers[i].worker.threadId) {
+        this.workers[i].status = WORKER_STATE_READY
+        break
+      }
+    }
+  }
+
+  teardown() {
+    for (let i = 0; i < this.workers.length; i++) {
+      this.workers[i].worker.unref()
+    }
   }
 }
 
