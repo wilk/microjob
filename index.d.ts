512b830bcc8465401c977fabae5dcff7cd47399b
wilk <wilk3ert@gmail.com>
2018-09-17 08:50:14 +0200

    config is optional

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
 import {Config} from './src/interfaces'
 
-export function job(handler: Function, config: Config): Promise<any>
+export function job(handler: Function, config?: Config): Promise<any>
 export function stop(): void
