052e362aafcc24eaa4b661f244dc0461f2d61097
wilk <wilk3ert@gmail.com>
2018-09-17 14:37:08 +0200

    regenerate docs

---
+++ docs/test/context.ts
@@ -0,0 +1,37 @@
+import helper from './helper'
+import { assert } from 'chai'
+import { job } from '../src/job'
+
+describe('Job Context Testing', () => {
+  it('should execute a an inline job with custom context', async () => {
+    let error
+    let res
+
+    class Foo {}
+
+    const ctx = {
+      hello: 'world',
+      fun: () => console.log('hello world!'),
+      numb: 10,
+      bool: true,
+      obj: {
+        nested: 'deep'/*,
+        date: new Date()*/
+      },
+      //date: new Date(),
+      arr: [10, 20, 'meh'],
+      myClass: Foo,
+      myInstance: new Foo()
+    }
+
+    try {
+      // @ts-ignore
+      res = await job(() => ({ hello, numb, bool, obj, arr/*, date*/ }), { ctx })
+    } catch (err) {
+      error = err
+    }
+
+    assert.isUndefined(error)
+    assert.deepEqual(res, { hello: ctx.hello, numb: ctx.numb, bool: ctx.bool, obj: ctx.obj, arr: ctx.arr })
+  })
+})
