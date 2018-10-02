ad801a7a8495c67cf8c48e173bd1aa9328a0a291
wilk <wilk3ert@gmail.com>
2018-10-02 23:08:47 +0200

    return void type for job function

--- src/job.ts
+++ src/job.ts
@@ -65,7 +65,7 @@ workerPool.on('tick', ({ work, worker }: {work: Task, worker: Worker}) => {
   }
 })
 
-export function job<T>(handler: <T>(data?: any) => T, config: Config = { ctx: {}, data: {} }): Promise<T> {
+export function job<T>(handler: <T>(data?: any) => void, config: Config = { ctx: {}, data: {} }): Promise<T> {
   return new Promise((resolve, reject) => {
     if (typeof handler !== 'function') return reject(new Error(MISSING_HANDLER_ERROR))
 
